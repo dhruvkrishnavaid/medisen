da40b2133296075b129d69137cf9f1547aed8ed4
kartikey2004-git <kartikeybhatnagar247@gmail.com>
2024-10-08 00:43:44 +0530

    created contact us page

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -7,7 +7,8 @@ const Contact = () => {
         <div className="mx-auto mb-10 lg:max-w-xl sm:text-center">
           <div className="pb-4 text-4xl font-bold w-full text-center">Contact Our Team</div>
           <p className="text-base text-gray-700 md:text-lg">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.
+            Contact us for more web insights<br></br> and if any kind of suggestion regarding to our project ,<br></br>{" "}
+            feel free to connect
           </p>
         </div>
         <div className="grid gap-10 mx-auto lg:grid-cols-2 lg:max-w-screen-lg">
@@ -15,21 +16,41 @@ const Contact = () => {
             <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
               <img
                 className="absolute object-cover w-full h-full rounded"
-                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                src="public\images\Screenshot 2024-10-07 211912.png"
                 alt="Person"
               />
             </div>
             <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
-              <p className="text-lg font-bold">Oliver Aguilerra</p>
-              <p className="mb-4 text-xs text-gray-800">Product Manager</p>
+              <p className="text-lg font-bold">Nikhil singh</p>
+              <p className="mb-4 text-xs text-gray-800">PRODUCT MANAGER</p>
               <p className="mb-4 text-sm tracking-wide text-gray-800">
-                Vincent Van Gogh’s most popular painting, The Starry Night.
+                Analyze the problem and suggest some projects ideas
+                <br></br>and holds the part of presentation
               </p>
               <div className="flex items-center space-x-3">
                 <a href="/" className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
                   <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                     <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
                   </svg>
+                  <div className=" flex items-center space-x-4 relative top-5">
+                    <big>
+                      <i className="fa-brands fa-instagram ">
+                        <a href="https://www.instagram.com/nikhhil_singhh/"></a>{" "}
+                      </i>
+                    </big>
+                    <big>
+                      <i className="fa-brands fa-linkedin">
+                        {" "}
+                        <a href="https://www.linkedin.com/in/nikhil-singh-819532260/"></a>{" "}
+                      </i>
+                    </big>
+                    <big>
+                      <i className="fa-brands fa-github">
+                        {" "}
+                        <a href="https://github.com/nikhil20-git"></a>
+                      </i>{" "}
+                    </big>
+                  </div>
                 </a>
                 <a href="/" className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
                   <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
@@ -43,21 +64,40 @@ const Contact = () => {
             <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
               <img
                 className="absolute object-cover w-full h-full rounded"
-                src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                src="public\images\Screenshot 2024-10-07 212420.png"
                 alt="Person"
               />
             </div>
             <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
-              <p className="text-lg font-bold">Marta Clermont</p>
-              <p className="mb-4 text-xs text-gray-800">Design Team Lead</p>
+              <p className="text-lg font-bold">Kartikey bhatnagar</p>
+              <p className="mb-4 text-xs text-gray-800">ASSISTANT TECH- LEAD</p>
               <p className="mb-4 text-sm tracking-wide text-gray-800">
-                Amet I love liquorice jujubes pudding croissant I love pudding.
+                Contribute for technical part and user interface and frontend side
               </p>
               <div className="flex items-center space-x-3">
                 <a href="/" className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
                   <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                     <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
                   </svg>
+                  <div className=" flex items-center space-x-4 relative top-5">
+                    <big>
+                      <i className="fa-brands fa-instagram ">
+                        <a href="https://www.instagram.com/_kartikey2004/"></a>{" "}
+                      </i>
+                    </big>
+                    <big>
+                      <i className="fa-brands fa-linkedin">
+                        {" "}
+                        <a href="https://www.linkedin.com/in/kartikey-bhatnagar-0a5a4432a/"></a>{" "}
+                      </i>
+                    </big>
+                    <big>
+                      <i className="fa-brands fa-github">
+                        {" "}
+                        <a href="https://github.com/kartikey2004-git"></a>
+                      </i>{" "}
+                    </big>
+                  </div>
                 </a>
                 <a href="/" className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
                   <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
@@ -71,21 +111,40 @@ const Contact = () => {
             <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
               <img
                 className="absolute object-cover w-full h-full rounded"
-                src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                src="public\images\Screenshot 2024-10-07 221229.png"
                 alt="Person"
               />
             </div>
             <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
-              <p className="text-lg font-bold">Alice Melbourne</p>
-              <p className="mb-4 text-xs text-gray-800">Human Resources</p>
+              <p className="text-lg font-bold">Diksha chauhan</p>
+              <p className="mb-4 text-xs text-gray-800">ASSISTANT PRODUCT MANAGER</p>
               <p className="mb-4 text-sm tracking-wide text-gray-800">
-                Lorizzle ipsum bling bling sit amizzle, consectetuer adipiscing elit.
+                Contribute in dataset and helps in building prototype
               </p>
               <div className="flex items-center space-x-3">
                 <a href="/" className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
                   <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                     <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
                   </svg>
+                  <div className=" flex items-center space-x-4 relative top-5">
+                    <big>
+                      <i className="fa-brands fa-instagram ">
+                        <a href="https://www.instagram.com/its.dika_chauhan/"></a>{" "}
+                      </i>
+                    </big>
+                    <big>
+                      <i className="fa-brands fa-linkedin">
+                        {" "}
+                        <a href="https://www.linkedin.com/in/diksha-chauhan-3b826a2aa?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"></a>{" "}
+                      </i>
+                    </big>
+                    <big>
+                      <i className="fa-brands fa-github">
+                        {" "}
+                        <a href="https://www.instagram.com/its.dika_chauhan/"></a>
+                      </i>{" "}
+                    </big>
+                  </div>
                 </a>
                 <a href="/" className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
                   <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
@@ -99,21 +158,40 @@ const Contact = () => {
             <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
               <img
                 className="absolute object-cover w-full h-full rounded"
-                src="https://images.pexels.com/photos/3931603/pexels-photo-3931603.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                src="public\images\WhatsApp Image 2024-10-07 at 22.21.00_5d56a372.jpg"
                 alt="Person"
               />
             </div>
             <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
-              <p className="text-lg font-bold">John Doe</p>
-              <p className="mb-4 text-xs text-gray-800">Good guy</p>
+              <p className="text-lg font-bold">Dhruv Vaid krishna </p>
+              <p className="mb-4 text-xs text-gray-800">TECH - LEAD</p>
               <p className="mb-4 text-sm tracking-wide text-gray-800">
-                Bacon ipsum dolor sit amet salami jowl corned beef, andouille flank.
+                Setup environment of our project with help of frameworks and also provide any type of technical support
               </p>
-              <div className="flex items-center space-x-3">
+              <div className="flex items-center  space-x-3">
                 <a href="/" className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
                   <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                     <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
                   </svg>
+                  <div className=" flex items-center space-x-4 relative top-5">
+                    <big>
+                      <i className="fa-brands fa-instagram ">
+                        <a href="https://instagram.com/dhruvkvaid" target="_blank"></a>{" "}
+                      </i>
+                    </big>
+                    <big>
+                      <i className="fa-brands fa-linkedin">
+                        {" "}
+                        <a href="https://www.linkedin.com/in/dhruv-krishna-vaid-677920328" target="_blank"></a>{" "}
+                      </i>
+                    </big>
+                    <big>
+                      <i className="fa-brands fa-github">
+                        {" "}
+                        <a href="https://github.com/dhruvkrishnavaid"></a>
+                      </i>{" "}
+                    </big>
+                  </div>
                 </a>
                 <a href="/" className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
                   <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
